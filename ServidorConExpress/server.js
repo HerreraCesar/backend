3c1104eb7215c6350ffb120ec71b081dcded3ad9
Herrera <herrera.cesar.arg@outlook.com>
2022-03-19 01:17:50 -0300

    Desafio 3

---
+++ ServidorConExpress/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const random = require('random')
+const Container = require('../ManejoDeArchivos/index')
+
+const products = new Container('./productos.txt')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const data = `
+        <h1>Bienvenido</h1>
+        <br/>
+        <a href="./productos">Todos los productos</a>
+        <br/>
+        <br/>
+        <a href="./productoRandom">Producto aleatorio</a>`
+    res.send(data)
+})
+
+app.get('/productos', async (req, res) => {
+    const data = await products.getAll()
+    res.send(data)
+})
+
+app.get('/productoRandom', async (req, res) => {
+    const data = await products.getAll()
+    randomIndex = random.int((min = 0), (max = data.length-1))
+    const id = data[randomIndex].id
+    const product = await products.getById(id)
+    res.send(product)
+})
+
+const PORT = 8080
+
+const server = app.listen( PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+})
+
+server.on('error', error => console.log(`Error en el servidor ${error}`))
+
